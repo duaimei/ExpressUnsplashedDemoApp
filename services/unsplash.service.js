14c1098806dfb91d14f2468bd89fe212976b279a
duaimei <adetwiler@optoro.com>
2019-11-07 11:26:26 -0500

    I got it to work by adding another requirement for toJson

--- services/unsplash.service.js
+++ services/unsplash.service.js
@@ -1,4 +1,5 @@
 const Unsplash = require('unsplash-js').default;
+const toJson = require('unsplash-js').toJson;
 
 const ACCESS_KEY = '';
 const SECRET_KEY = ''
@@ -7,13 +8,12 @@ const SECRET_KEY = ''
 
 const unsplash = new Unsplash({
   accessKey: ACCESS_KEY,
-  secret: SECRET_KEY,
-  callbackUrl: 'http://localhost:3000/'
+  secret: SECRET_KEY
 });
 
 exports.query = function (res) {
   unsplash.search.collections("cats", 1)
-  // .then(unsplash.toJson)
+  .then(toJson)
   .then(json => {
     console.log(json)
     res.status(200).json(json)
